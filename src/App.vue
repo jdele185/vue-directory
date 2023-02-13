ae500a11f87db7697887fe3a8dfa29b01bb7d680
Juan de Leon <jdele185@stu.southtexascollege.edu>
2023-02-13 13:53:39 -0600

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
 import NavMenu from '@/components/NavMenu.vue'
-import MainPage from '@/components/MainPage.vue'
 </script>
 
 <template>
   <NavMenu />
-  <MainPage />
+  <RouterView />
 </template>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 class="not-found">Can't Find It!</h2>
+</template>
+
+<style lang="postcss" scoped>
+.not-found {
+  @apply pt-80 text-center text-5xl font-bold;
+}
+</style>
