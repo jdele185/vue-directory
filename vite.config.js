402894de9d6dec16949ea3c159391c5647343250
Juan de Leon <jdele185@stu.southtexascollege.edu>
2023-05-02 12:54:56 -0500

    Reboot Changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server:{
+    https: true
+  },
+  plugins: [vue(), mkcert(p)],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
